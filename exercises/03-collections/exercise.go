ebc73709e5434565ca290122c829e41adebd9433
LDKhangg <lekhangmc12@gmail.com>
2026-07-30 00:26:22 +0700

    exercise 3

--- exercises/03-collections/exercise.go
+++ exercises/03-collections/exercise.go
@@ -1,9 +1,23 @@
 package collections
 
 func UniqueWords(words []string) []string {
-	return nil
+	seen := make(map[string]bool)
+	var result []string
+
+	for _, word := range words {
+		if !seen[word] {
+			seen[word] = true
+			result = append(result, word)
+		}
+	}
+
+	return result
 }
 
 func SumArray(numbers [4]int) int {
-	return 0
+	var total int
+	for i := 0; i < 4; i++ {
+		total += numbers[i]
+	}
+	return total
 }
