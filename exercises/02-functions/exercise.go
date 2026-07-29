0c29e658e136b95cdf503b8ad3e90b27648d141b
LDKhangg <lekhangmc12@gmail.com>
2026-07-30 00:02:15 +0700

    exercise 2

--- exercises/02-functions/exercise.go
+++ exercises/02-functions/exercise.go
@@ -5,7 +5,15 @@ import "errors"
 var ErrDivideByZero = errors.New("cannot divide by zero")
 
 func Divide(dividend, divisor int) (quotient, remainder int, err error) {
-	return 0, 0, nil
+	if divisor == 0 {
+		return 0, 0, ErrDivideByZero
+	}
+
+	quotient = dividend / divisor
+
+	remainder = dividend % divisor
+
+	return quotient, remainder, nil
 }
 
-func Double(number *int) {}
+func Double(number *int) { *number = *number * 2 }
