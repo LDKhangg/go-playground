de208e5bf1affb53d72aeb437ac432f7d4ddc1ef
LDKhangg <lekhangmc12@gmail.com>
2026-08-04 22:59:46 +0700

    exercise 0.3

--- exercises/00-syntax-drills/03-strings-runes/exercise.go
+++ exercises/00-syntax-drills/03-strings-runes/exercise.go
@@ -1,5 +1,16 @@
 package runes
 
+import "strings"
+
 func Initials(name string) string {
-	panic("TODO")
+	words := strings.Fields(name)
+	var sb strings.Builder
+
+	for _, word := range words {
+		for _, char := range word {
+			sb.WriteRune(char)
+			break
+		}
+	}
+	return sb.String()
 }
