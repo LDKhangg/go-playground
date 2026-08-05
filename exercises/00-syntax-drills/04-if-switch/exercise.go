982521f3712993146f7af75db36f0bad6eef3d46
LDKhangg <lekhangmc12@gmail.com>
2026-08-05 20:24:09 +0700

    exercise 0.4

--- exercises/00-syntax-drills/04-if-switch/exercise.go
+++ exercises/00-syntax-drills/04-if-switch/exercise.go
@@ -1,5 +1,16 @@
 package branching
 
 func ClassifyScore(score int) string {
-	panic("TODO")
+	switch {
+	case score < 0:
+		return "invalid"
+	case score < 50:
+		return "fail"
+	case score < 85:
+		return "pass"
+	case score < 101:
+		return "excellent"
+	default:
+		return "invalid"
+	}
 }
