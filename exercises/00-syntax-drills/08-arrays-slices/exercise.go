5cc6e902fc13a76664165e5a855be90ab8ffc0ff
LDKhangg <lekhangmc12@gmail.com>
2026-08-06 15:34:04 +0700

    exercise 0.8

--- exercises/00-syntax-drills/08-arrays-slices/exercise.go
+++ exercises/00-syntax-drills/08-arrays-slices/exercise.go
@@ -1,9 +1,35 @@
 package slices
 
+import "fmt"
+
 func InsertAt(values []int, index int, value int) ([]int, error) {
-	panic("TODO")
+	if index < 0 || index > len(values) {
+		return nil, fmt.Errorf(
+			"index %d out of range [0,%d]",
+			index,
+			len(values),
+		)
+	}
+	newArr := make([]int, 0, len(values)+1)
+
+	newArr = append(newArr, values[:index]...)
+	newArr = append(newArr, value)
+	newArr = append(newArr, values[index:]...)
+
+	return newArr, nil
 }
 
 func RemoveAt(values []int, index int) ([]int, error) {
-	panic("TODO")
+	if index < 0 || index > len(values) {
+		return nil, fmt.Errorf(
+			"index %d out of range [0,%d]",
+			index,
+			len(values),
+		)
+	}
+	newArr := make([]int, 0, len(values)-1)
+
+	newArr = append(newArr, values[:index]...)
+	newArr = append(newArr, values[index+1:]...)
+	return newArr, nil
 }
