5ddab3d8468633143ad94f64abe77cd62f1a2b7b
LDKhangg <lekhangmc12@gmail.com>
2026-08-06 22:50:58 +0700

    exercise 0.15

--- exercises/00-syntax-drills/15-json-basics/exercise.go
+++ exercises/00-syntax-drills/15-json-basics/exercise.go
@@ -1,9 +1,24 @@
 package jsonbasics
 
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
 type Task struct {
 	Title string `json:"title"`
 }
 
 func DecodeTask(data []byte) (Task, error) {
-	panic("TODO")
+	var m Task
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return Task{}, err
+	}
+	trimmedTitle := strings.TrimSpace(m.Title)
+	if trimmedTitle == "" {
+		return Task{}, errors.New("Invalid title")
+	}
+	return m, nil
 }
