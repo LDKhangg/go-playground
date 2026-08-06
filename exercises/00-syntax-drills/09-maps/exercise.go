08848213d9aafd3c2eddcef9ac6f46e98f2dd3b6
LDKhangg <lekhangmc12@gmail.com>
2026-08-06 16:13:03 +0700

    exercise 0.9

--- exercises/00-syntax-drills/09-maps/exercise.go
+++ exercises/00-syntax-drills/09-maps/exercise.go
@@ -1,9 +1,18 @@
 package mapsx
 
+import "strings"
+
 func CountWords(text string) map[string]int {
-	panic("TODO")
+	words := strings.Fields(text)
+	res := make(map[string]int)
+	for _, word := range words {
+		res[word]++
+	}
+	return res
 }
 
 func LookupScore(scores map[string]int, name string) (int, bool) {
-	panic("TODO")
+	value, ok := scores[name]
+
+	return value, ok
 }
